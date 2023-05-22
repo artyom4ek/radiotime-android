771b5df92040402ac635e571f91c3d79a34556b4
Artem Vasylenko <avasylenko@revenuewell.com>
2023-05-22 13:27:23 +0300

    Add playback error handling

--- app/src/main/java/com/tunein/radiotime/ui/main/media/PlaybackManager.kt
+++ app/src/main/java/com/tunein/radiotime/ui/main/media/PlaybackManager.kt
@@ -1,6 +1,7 @@
 package com.tunein.radiotime.ui.main.media
 
 interface PlaybackManager {
+    fun handlePlaybackError(onPlaybackError: (String) -> Unit)
     fun play(url: String)
     fun play()
     fun pause()
