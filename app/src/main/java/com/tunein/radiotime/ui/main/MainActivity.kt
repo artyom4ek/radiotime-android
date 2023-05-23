cbd7cd7ea05724c23fbcbdac24ae035919719d66
Artem Vasylenko <avasylenko@revenuewell.com>
2023-05-23 18:05:08 +0300

    Move Graph logic to graph package

--- app/src/main/java/com/tunein/radiotime/ui/main/MainActivity.kt
+++ app/src/main/java/com/tunein/radiotime/ui/main/MainActivity.kt
@@ -1,7 +1,5 @@
 package com.tunein.radiotime.ui.main
 
-import javax.inject.Inject
-
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -9,9 +7,10 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
+
+import javax.inject.Inject
 
 import dagger.hilt.android.AndroidEntryPoint
 
